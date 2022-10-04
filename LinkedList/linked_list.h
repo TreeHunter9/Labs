7794f51f58c68122ca09c1426fd5d98eaafbdcb7
Artem Ivanov <lavolp1.lan@yandex.ru>
2022-10-04 14:40:22 +0300

    Implement functions

--- LinkedList/linked_list.h
+++ LinkedList/linked_list.h
@@ -1,96 +1,241 @@
 #pragma once
 
 template<class T>
+class LinkedListNode
+{
+public:
+	LinkedListNode(const T& value)
+		: value(value), next(nullptr), prev(nullptr)
+	{}
+public:
+	T value;
+private:
+	LinkedListNode<T>* next;
+	LinkedListNode<T>* prev;
+};
+
+template<class T>
+class LinkedListIterator
+{
+public:
+	friend LinkedListNode<T>;
+
+	using ValueType = LinkedListNode<T>;
+	using PointerType = ValueType*;
+	using ReferenceType = ValueType&;	
+public:
+	LinkedListIterator(PointerType ptr)
+		: m_ptr(ptr)
+	{}
+
+	LinkedListIterator<T>& operator++()
+	{
+		m_ptr = m_ptr->next;
+		return *this;
+	}
+
+	LinkedListIterator<T>& operator++(int) 
+	{
+		LinkedListIterator<T> tmp = *this;
+		++(*this);
+		return tmp;
+	}
+
+	LinkedListIterator<T>& operator--()
+	{
+		m_ptr = m_ptr->prev;
+		return *this;
+	}
+
+	LinkedListIterator<T>& operator--(int)
+	{
+		LinkedListIterator<T> tmp = *this;
+		--(*this);
+		return tmp;
+	}
+
+	PointerType operator->()
+	{
+		return m_ptr;
+	}
+
+	ReferenceType operator*() const
+	{
+		return *m_ptr;
+	}
+
+	bool operator==(const LinkedListIterator<T>& other) const
+	{
+		return m_ptr == other.m_ptr;
+	}
+
+	bool operator!=(const LinkedListIterator<T>& other) const
+	{
+		return !(*this == other);
+	}
+private:
+	PointerType m_ptr;
+};
+
+template<class T>
 class LinkedList
 {
 public:
-	template<class T>
-	class LinkedListNode
+	friend LinkedListNode<T>;
+public:
+	LinkedList()
+		: m_first(nullptr), m_last(nullptr)
+	{}
+
+	~LinkedList()
 	{
-	public:
-		LinkedListNode(const T& value)
-			: value(value)
-		{}
-	public:
-		T* value;
-		LinkedListNode* next;
-		LinkedListNode* prev;
-	};
-
-	template<class T>
-	class Iterator
-	{
-		using value_type = LinkedListNode<T>;
-		using pointer = LinkedListNode<T>*;
-		using reference = LinkedListNode<T>&;
-	public:
-		Iterator(pointer ptr)
-			: m_ptr(ptr)
-		{}
-
-		reference operator*() const
-		{
-			return *m_ptr;
-		}
-
-		pointer operator->()
-		{
-			return m_ptr;
-		}
-
-		Iterator& operator++()
-		{
-			m_ptr = m_ptr->next;
-			return *this;
-		}
-
-		Iterator operator++(int) 
-		{
-			Iterator tmp = *this;
-			++(*this);
-			return tmp;
-		}
-
-		template<class T>
-		friend bool operator==(const Iterator<T>& a, const Iterator<T>& b)
-		{
-			return a.m_ptr == b.m_ptr;
-		}
-
-		friend bool operator!=(const Iterator<T>& a, const Iterator<T>& b)
-		{
-			return !(a == b);
-		}
-	private:
-		pointer m_ptr;
-	};
-
-	LinkedList();
-	~LinkedList();
-
-	void AddBegin(const T& value);
-	void AddEnd(const T& value);
-	void RemoveBegin();
-	void RemoveEnd();
-	void RemoveAll();
-	T* GetBegin();
-	T* GetEnd();
-	void Remove(const T& value);
-	bool HasItem(const T& value);
-
-	Iterator<T> begin()
-	{
-		return Iterator(m_first);
+		RemoveAll();
 	}
-	Iterator<T> end()
+
+	void AddBegin(const T& value)
 	{
-		return Iterator(nullptr);
+		LinkedListNode<T>* node = new LinkedListNode(value);
+		if (!m_first)
+		{
+			m_first = node;
+			m_last = node;
+			return;
+		}
+
+		node->next = m_first;
+		m_first->prev = node;
+		m_first = node;
+	}
+
+	void AddEnd(const T& value)
+	{
+		LinkedListNode<T>* node = new LinkedListNode(value);
+		if (!m_first)
+		{
+			m_first = node;
+			m_last = node;
+			return;
+		}
+
+		node->prev = m_last;
+		m_last->next = node;
+		m_last = node;
+	}
+	
+	void RemoveBegin()
+	{
+		if (!m_first)
+			return;
+		
+		if (m_first == m_last)
+		{
+			delete m_first;
+			m_first = nullptr;
+			m_last = nullptr;
+			return;
+		}
+
+		LinkedListNode<T>* delNode = m_first;
+		m_first = m_first->next;
+		m_first->prev = nullptr;
+		delete delNode;
+	}
+
+	void RemoveEnd()
+	{
+		if (!m_first)
+			return;
+
+		if (m_first == m_last)
+		{
+			delete m_first;
+			m_first = nullptr;
+			m_last = nullptr;
+			return;
+		}
+
+		LinkedListNode<T>* delNode = m_last;
+		m_last = m_last->prev;
+		m_last->next = nullptr;
+		delete delNode;
+	}
+
+	void Remove(const T& value)
+	{
+		LinkedListNode<T>* foundNode = FindNode(value);
+		if (!foundNode)
+			return;
+
+		LinkedListNode<T>* prevNode = foundNode->prev;
+		LinkedListNode<T>* nextNode = foundNode->next;
+
+		if (!prevNode)
+		{
+			RemoveBegin();
+			return;
+		}
+		if (!nextNode)
+		{
+			RemoveEnd();
+			return;
+		}
+		prevNode->next = nextNode;
+		nextNode->prev = prevNode;
+		delete foundNode;
+	}
+	
+	void RemoveAll()
+	{
+		while (m_last)
+		{
+			RemoveEnd();
+		}
+	}
+
+	T GetBegin() const
+	{
+		return m_first->value;
+	}
+
+	T GetEnd() const
+	{
+		return m_last->value;
+	}
+
+	bool HasItem(const T& value) const
+	{
+		return FindNode(value);
+	}
+
+	bool IsEmpty() const
+	{
+		return !m_first;
+	}
+
+	LinkedListIterator<T> begin() const
+	{
+		return LinkedListIterator<T>(m_first);
+	}
+
+	LinkedListIterator<T> end() const
+	{
+		return LinkedListIterator<T>(nullptr);
 	}
 private:
-	LinkedListNode<T>* FindNode(const T& value);
+	LinkedListNode<T>* FindNode(const T& value)
+	{
+		LinkedListNode<T>* node = m_first;
+		while (node != nullptr)
+		{
+			if (node->value == value)
+				return node;
+
+			node = node->next;
+		}
+
+		return nullptr;
+	}
 private:
 	LinkedListNode<T>* m_first;
 	LinkedListNode<T>* m_last;
 };
-
-
--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -3,7 +3,14 @@
 
 int main()
 {
-	LinkedList<int> list;
-	list.AddBegin(5);
-	list.AddEnd(6);
+	LinkedList<int> linked_list;
+	linked_list.AddBegin(5);
+	linked_list.AddEnd(6);
+	linked_list.AddEnd(23);
+	linked_list.AddBegin(3);
+	linked_list.Remove(3);
+	for (const auto& node : linked_list)
+	{
+		std::cout << node.value << " ";
+	}
 }
